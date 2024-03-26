7a7e510720e0a45a37a448214b22236555b44cb9
Bohdan Khmil <khmil.work@gmail.com>
2024-03-26 20:32:47 +0200

    created the basic structure, wrote the first part of the code

---
+++ main.js
@@ -0,0 +1,54 @@
+class User {
+    constructor(id, name, idTagType, nameTagType, cssClasses, parentHTML) {
+        this.id = id;
+        this.nameOfUser = name;
+        this.idTagType = idTagType;
+        this.nameTagType = nameTagType;
+        this.cssClasses = cssClasses;
+        this.parentHTML = parentHTML;
+    }
+
+    build() {
+        const divWrapper = document.createElement('div');
+        divWrapper.classList.add(this.cssClasses[3]);
+
+        const idField = document.createElement(this.idTagType);
+        idField.classList.add(this.cssClasses[0]);
+        idField.innerText = 'id: ' + this.id;
+
+        const nameField = document.createElement(this.nameTagType);
+        nameField.classList.add(this.cssClasses[1]);
+        nameField.innerText = 'name: ' + this.nameOfUser;
+
+        const btnDetails = document.createElement('button');
+        btnDetails.classList.add(this.cssClasses[2]);
+        btnDetails.innerText = 'View user details';
+
+        btnDetails.onclick = () => {
+            window.location.href = './src/user_details/user-details.html?id=' + this.id;
+        };
+
+        divWrapper.append(idField, nameField, btnDetails);
+        this.parentHTML.appendChild(divWrapper);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    localStorage.setItem('isLogin', 'true');
+    if (!localStorage.getItem('isLogin') || localStorage.getItem('isLogin') === 'false') {
+        // setTimeout(() => window.location.href = './src/login/login.html', 1000);
+        window.location.href = './src/login/login.html';
+    } else {
+        const root = document.createElement('div');
+        root.classList.add('root');
+        document.body.appendChild(root);
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(dataArray => {
+                dataArray.forEach(({id, name}) => {
+                    new User(id, name, 'h1', 'h1', ['id', 'name', 'btn-details', 'user'], root).build();
+                });
+            });
+    }
+});
---
+++ src/login/login.js
@@ -0,0 +1,69 @@
+const divMainWrapper = document.createElement('div');
+divMainWrapper.classList.add('main-wrapper');
+
+const divBlock = document.createElement('div');
+divBlock.classList.add('main');
+
+const h1Title = document.createElement('h1');
+h1Title.classList.add('title');
+h1Title.innerText = 'LOGIN';
+
+        // form start
+const form = document.createElement('form');
+
+            // email
+const labelEmail = document.createElement('label');
+labelEmail.setAttribute('for', 'email');
+labelEmail.classList.add('block', 'form-font-size');
+labelEmail.innerText = 'e-mail';
+
+const inputEmail = document.createElement('input');
+inputEmail.setAttribute('id', 'email');
+inputEmail.classList.add('block', 'form-font-size');
+inputEmail.placeholder = 'example@gmail.com';
+            // email end
+
+            // password start
+const labelPassword = document.createElement('label');
+labelPassword.setAttribute('for', 'password');
+labelPassword.classList.add('block', 'form-font-size');
+labelPassword.innerText = 'password';
+
+const inputPassword = document.createElement('input');
+inputPassword.type = 'password';
+inputPassword.setAttribute('id', 'password');
+inputPassword.classList.add('block', 'form-font-size');
+inputPassword.placeholder = '********';
+            // password end
+
+            // checkBox start
+const divCheckBoxWrapper = document.createElement('div');
+
+const checkBox = document.createElement('input');
+checkBox.setAttribute('id', 'remember');
+checkBox.type = 'checkBox';
+
+const labelCheckBox = document.createElement('label');
+labelCheckBox.setAttribute('for', 'remember');
+labelCheckBox.classList.add('form-font-size');
+labelCheckBox.innerText = 'Remember me';
+            // checkBox end
+
+const btnSend = document.createElement('button');
+btnSend.innerText = 'LOGIN';
+
+btnSend.onclick = e => {
+    e.preventDefault();
+
+    window.location.href = '../../index.html';
+};
+    // form end
+
+const divCompany = document.createElement('div');
+
+// APPENDINGS
+divCheckBoxWrapper.append(checkBox, labelCheckBox);
+form.append(labelEmail, inputEmail, labelPassword, inputPassword, divCheckBoxWrapper, btnSend);
+divBlock.append(h1Title, form, divCompany);
+divMainWrapper.appendChild(divBlock);
+document.body.appendChild(divMainWrapper);
